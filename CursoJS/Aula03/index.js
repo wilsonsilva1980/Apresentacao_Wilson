8f55474bd64e3e54c066b4f14a31ba729e556850
wilsonsilva1980 <willhalls2005@outlook.com>
2026-04-23 21:31:18 -0300

    push files

---
+++ CursoJS/Aula03/index.js
@@ -0,0 +1,11 @@
+console.debug('Hello world');
+
+console.debug('Hello world');
+console.debug("Hello world");
+console.debug(`Hello world`);
+
+console.debug('Hello "world"');
+console.debug("Hello 'world'");
+console.debug(`'Hello' "world"`);
+
+console.debug(35, 35.15, 'Hello world');
